0791452498719afb7409d1f723dbea2ec26d56f1
Dave Pearson <davep@davep.org>
2017-08-16 10:35:09 +0100

    v1.1

--- eg.el
+++ eg.el
@@ -2,7 +2,7 @@
 ;; Copyright 2017 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.0
+;; Version: 1.1
 ;; Keywords: docs
 ;; URL: https://github.com/davep/eg.el
 ;; Package-Requires: ((cl-lib "0.5") (emacs "24.3"))
